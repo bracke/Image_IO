e3fcdc57ed2f868e3ad21aeb3330d23c5ec3dbe5
Bent Bracke <bent@bracke.dk>
2024-08-12 17:22:34 +0200

    Merged changes from original (PNG related)

--- src/image_io-operations.ads
+++ src/image_io-operations.ads
@@ -1,4 +1,4 @@
--- Image I/O: output in PPM, BMP, and QOI formats; input in BMP, GIF, JPG, PNG, PNM, QOI, and TGA formats
+-- Image I/O: output in PPM, BMP, PNG, and QOI formats; input in BMP, GIF, JPG, PNG, PNM, QOI, and TGA formats
 -- Copyright (C) by Pragmada Software Engineering
 -- Released under the terms of the BSD 3-Clause license; see https://opensource.org/licenses
 
@@ -21,6 +21,12 @@ package Image_IO.Operations is
 
    procedure Write_QOI (File_Name : in String; Image : in Image_Data);
    -- Creates a file named Name; writes Image to it in QOI format; and closes the file
+
+   procedure Write_PNG (File_Name : in String; Image : in Image_Data; Grayscale : in Boolean := False);
+   -- Creates a file named Name; writes Image to it in PNG format; and closes the file
+   -- If Grayscale, treats Image as a grayscale image, with all pixels having equal Red, Green, and Blue components. The Red
+   -- component is the only one used for the output
+   -- Otherwise, treats Image as a color image
 
    procedure Read (Name : in String; Image : in out Holders.Handle) with
       Pre => Ada.Directories.Exists (Name) and then Ada.Directories.Kind (Name) in Ada.Directories.Ordinary_File;
